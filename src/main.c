9792371990b427952f8fe4fab881e67466143859
NASCC <pdbt.contact@gmail.com>
2024-08-10 10:23:22 -0300

    [ADDED/TESTED] testing a simple c program

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/* ------------------------------------------------------------------
+ *
+ * main.c
+ *
+ *     file responsible for store the main task and / or the
+ *     combination of all tasks
+ ------------------------------------------------------------------*/
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  printf("Hello World :^D\n");
+
+  return 0;
+}
