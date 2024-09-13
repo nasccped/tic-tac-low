8cd546494da8ac6ab01f5b86e3f5287c3ab1d555
NASCC <pedro.main.tec@gmail.com>
2024-09-12 22:27:11 -0300

    [FIXED] minor comment fix

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
  *
  *     file responsible for store the main task and / or the
  *     combination of all tasks
- ------------------------------------------------------------------*/
+ * ----------------------------------------------------------------*/
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
