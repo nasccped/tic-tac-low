3154ea2c81dd04f6258242952abed4d1d8643f1b
NASCC <pedro.main.tec@gmail.com>
2024-09-26 14:46:53 -0300

    [UPDATED] To fix: char* insertion when int is expected

--- src/main.c
+++ src/main.c
@@ -7,10 +7,18 @@
  * ----------------------------------------------------------------*/
 #include <stdio.h>
 
+// main function
 int main(int argc, char *argv[]) {
 
+  // initialize an empty int variable
   int number;
 
+  /*
+    FIXME: cool block of code. Some kind of question is done,
+           you enter a int type value such as 10, 32, 54, etc...
+           but, if you enter like a char `a`, some bug behaviours
+           occurs
+  */
   do {
 
     printf("Insert a number [1 - 10]: ");
