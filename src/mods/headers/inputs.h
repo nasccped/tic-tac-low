28341c4dc52c79f5f3e1bd31d72e9d4661376c1f
NASCC <pedro.main.tec@gmail.com>
2024-09-27 18:39:31 -0300

    [FIXED] Removing #include's. They're at '_includes.h' file

--- src/mods/headers/inputs.h
+++ src/mods/headers/inputs.h
@@ -1,26 +1,6 @@
 #ifndef _INPUTS_H_
 #define _INPUTS_H_
 
-// if <stdlib.h> isn't already imported
-#ifndef _STDLIB_H_
-  #include <stdlib.h>
-#endif
-
-
-// if <stdio.h> isn't already imported
-#ifndef _STDIO_H_
-  #include <stdio.h>
-#endif
-
-// if <ctype.h> isn't already imported
-#ifndef _CTYPE_H_
-  #include <ctype.h>
-#endif
-
-/*
-This function returns a String (char*) by user's command line
-response
-*/
 
 char* get_user_response();
 
