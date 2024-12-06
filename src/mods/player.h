802e52a6307ab2254362909f82c28e6ada9c40a1
NASCC <pedro.main.tec@gmail.com>
2024-12-06 20:08:40 -0300

    [ADDED] Header file to store player related content

---
+++ src/mods/player.h
@@ -0,0 +1,25 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+#define PLAYER_NAME_MAX_LEN 30
+#define PLAYER_LIST_MAX_SIZE 5
+
+typedef struct Player {
+  char name[PLAYER_NAME_MAX_LEN];
+  unsigned points;
+} Player;
+
+typedef struct PlayerList {
+  Player *list[PLAYER_LIST_MAX_SIZE];
+  unsigned current_lenght, max_length;
+  bool (*insert_player)(struct LinkedPlayers *, Player *);
+} PlayerList;
+
+Player *new_player(char *name);
+bool insert_player(PlayerList *self, Player *player);
+void destroy_player(Player *player);
+void destroy_player_list(PlayerList *list);
+
+extern PlayerList PLAYER_LIST;
+
+#endif
