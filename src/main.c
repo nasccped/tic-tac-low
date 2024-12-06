bb894026a0d961e35b927c0e05af7973ddb71d7c
NASCC <pedro.main.tec@gmail.com>
2024-12-06 20:08:01 -0300

    [UPDATED] Including the header file

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#ifndef __INCLUDE__H_
+#include "./_include_.h"
+#endif
 
 int main(int arc, char *argv[]) {
 
   char red[] = "\033[1;32m";
   char none[] = "\033[0m";
 
-  printf("\n%sAyooo%s, World!\n", red, none); 
+  printf("\n%sAyooo%s, World!\n", red, none);
 
   return 0;
 }
