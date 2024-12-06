9f9f4a337f2d0b42c0de3a69b2debc11249e0ee0
NASCC <pedro.main.tec@gmail.com>
2024-12-06 20:07:08 -0300

    [ADDED] New includes all in one

---
+++ src/_include_.h
@@ -0,0 +1,20 @@
+#ifndef __INCLUDE__H_
+#define __INCLUDE__H_
+
+#ifndef _STDIO_H_
+#include <stdio.h>
+#endif
+
+#ifndef _STDBOOL_H_
+#include <stdbool.h>
+#endif
+
+#ifndef _STRING_H_
+#include <string.h>
+#endif
+
+#ifndef _STDLIB_H_
+#include <stdlib.h>
+#endif
+
+#endif
