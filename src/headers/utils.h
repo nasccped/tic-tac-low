016bed6cff1f8d4397429567d420d2420bbd0d7e
NASCC <pedro.main.tec@gmail.com>
2025-01-09 20:07:01 -0300

    [FIXED] Removing useless param names

--- src/headers/utils.h
+++ src/headers/utils.h
@@ -5,8 +5,8 @@
 int yes_or_no_input();
 
 // char* (string) manipulation
-int is_alpha_str(char *from);
-void str_to_lower(char *dest);
-char *strip(char *from, unsigned m_len);
+int is_alpha_str(char *);
+void str_to_lower(char *);
+char *strip(char *, unsigned);
 
 #endif
