47f5993c416740bac1d779f3e32e9d66b88c996a
NASCC <pedro.main.tec@gmail.com>
2025-01-09 14:28:11 -0300

    [ADDED] Hold and export consts/vars

---
+++ src/headers/const_vars.h
@@ -0,0 +1,11 @@
+#ifndef _CONST_VARS_H_
+#define _CONST_VARS_H_
+
+static const char *RESET_ESCAPE = "\x1b[0m";
+static const char *BOLD_RED_NONE = "\x1b[1;31m";
+static const char *BOLD_GREEN_NONE = "\x1b[1;32m";
+static const char *BOLD_YELLOW_NONE = "\x1b[1;33m";
+
+extern unsigned ENABLE_COLORS;
+
+#endif
