892150eefc14486ff7c4344ec2eb2eb80b116735
NASCC <pedro.main.tec@gmail.com>
2025-01-09 14:29:09 -0300

    [UPDATED] Implementing value extraction from header

--- src/main.c
+++ src/main.c
@@ -1,12 +1,25 @@
 #include "./headers/__INCLUDE__.H"
+#include "./headers/const_vars.h"
 #include "./headers/visuals.h"
 
 int main(int argc, char *argv[]) {
 
+  const char *COLORS[] = {
+      BOLD_RED_NONE,
+      BOLD_GREEN_NONE,
+      BOLD_YELLOW_NONE,
+  };
+
   clear_terminal();
 
-  printf("\n");
-  printf("  Hello, World!\n");
+  char *hlwd = "  Hello, World!";
+  size_t len = strlen(hlwd);
+
+  printf("\n  ");
+  for (int i = 0; i < len; i++) {
+    printf("%s%c", COLORS[i % 3], hlwd[i]);
+  }
+  printf("%s\n", RESET_ESCAPE);
 
   return 0;
 }
