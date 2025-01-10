53c7d96a90c9fca9299a61c932c1aafcc957ac2d
NASCC <pedro.main.tec@gmail.com>
2025-01-10 12:37:07 -0300

    [UPDATED/TESTED] Preparing main function to receive test args

--- src/main.c
+++ src/main.c
@@ -14,10 +14,21 @@
 #include "./headers/game_options.h"
 #endif
 
+#ifndef _STRING_H
+#include <string.h>
+#endif
+
 unsigned ENABLE_COLORS;
+
+void test();
 
 // main function
 int main(int argc, char *argv[]) {
+
+  if (argc == 2 && (strcmp(argv[1], "test") == 0)) {
+    test();
+    return 0;
+  }
 
   // storing yes/no user responsa as int
   int color_trigger;
@@ -50,4 +61,11 @@ int main(int argc, char *argv[]) {
   print_options(ENABLE_COLORS, MENU_OPTIONS);
 
   return 0;
+}
+
+void test() {
+
+  printf("\n");
+  printf("  This is the current test function.   xD\n");
+  printf("\n");
 }
