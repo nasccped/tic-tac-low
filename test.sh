1d10de95c7a2e962ded6df7947c8d9de01041809
NASCC <pedro.main.tec@gmail.com>
2025-01-10 15:56:13 -0300

    [UPDATED] Test file full implementation

--- test.sh
+++ test.sh
@@ -12,4 +12,49 @@
 # Unfortunately, this will work only for unix-like pc's or machines
 # that contains the bash tool
 
-echo "Hi there. I'm a test Bash file!!!"
+# getting values from cmd line/cur dir
+all_sources=$(ls src/*.c)
+final_file="./out/tic-tac-low-test"
+args_count=$#
+arg=$1
+
+# clear terminal and prepare to print content
+clear
+
+# if invalid argc
+if [ 1 -ne $args_count ]; then
+  echo
+  echo "  Invalid arg count: (received $args_count, expected: 1)"
+  echo "  Exiting program..."
+  echo
+  exit 0
+fi
+
+# else, continue
+echo
+echo "  Preparing for test..."
+echo "  Let's compile: "
+
+# show each file being compiled
+for fl in $all_sources; do
+  echo "      > $fl"
+done
+
+# if test file already exists
+if [ -f $final_file ]; then \
+  rm $final_file
+fi
+
+# compile
+gcc $all_sources -o $final_file;
+
+# echo separator
+echo
+echo "  --------------------------------------------------------------------"
+
+# if test file exists (successfuly compiled)
+if [ -f $final_file ]; then
+  $final_file $arg
+fi
+
+echo
