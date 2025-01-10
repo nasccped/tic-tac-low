a18b2b5ee300768f4027ce0dfcd3f2e488ae1b6d
NASCC <pedro.main.tec@gmail.com>
2025-01-10 12:44:28 -0300

    [ADDED] New test script

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this is a fast build-run bash file specific for program tests
+# to turn this on, type:
+#
+# $ chmod u+x test.sh
+#
+# and to run, just type:
+#
+# # ./test.sh
+#
+# Unfortunately, this will work only for unix-like pc's or machines
+# that contains the bash tool
+
+echo "Hi there. I'm a test Bash file!!!"
