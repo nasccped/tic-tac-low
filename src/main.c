f4cd96892fabb1eb80b04a893d563147b71206fa
NASCC <pedro.main.tec@gmail.com>
2025-01-10 10:46:45 -0300

    [TESTING] Testing implemented features

--- src/main.c
+++ src/main.c
@@ -2,27 +2,52 @@
 #include <stdio.h>
 #endif
 
-#include "./headers/utils.h"
+#ifndef _VISUALS_H
 #include "./headers/visuals.h"
+#endif
+
+#ifndef _UTILS_H
+#include "./headers/utils.h"
+#endif
+
+#ifndef _GAME_OPTIONS_H
+#include "./headers/game_options.h"
+#endif
 
 unsigned ENABLE_COLORS;
 
+// main function
 int main(int argc, char *argv[]) {
+
+  // storing yes/no user responsa as int
+  int color_trigger;
 
   clear_terminal();
 
+  // asking for colors
   printf("\n");
-
-  printf("  Enable ANSI basic colors? ( yes / no )\n");
+  printf("  Did you want to enable colors in the program? (yes / no | anything to quit)\n");
   printf("  > ");
 
-  int sec;
-  ENABLE_COLORS = ((sec = yes_or_no_input()) == -1) ? 0 : sec;
+  // if quit was called
+  if ((color_trigger = yes_or_no_input()) == -1) {
 
-  printf("\n");
-  printf("  The program colors is %s\n",
-         (ENABLE_COLORS) ? "\x1b[1;34mENABLED\x1b[0m" : "DISABLED");
-  printf("\n");
+    clear_terminal();
+
+    // quit print
+    printf("\n");
+    printf("  Quitting...\n");
+
+    // terminate
+    return 0;
+  }
+
+  // update var (only > -1 values)
+  ENABLE_COLORS = color_trigger;
+
+  clear_terminal();
+  print_banner(ENABLE_COLORS);
+  print_options(ENABLE_COLORS, MENU_OPTIONS);
 
   return 0;
 }
