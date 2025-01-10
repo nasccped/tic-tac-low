e64b4748db7d023468b6e2b6e8a5c03b38cb259d
NASCC <pedro.main.tec@gmail.com>
2025-01-10 10:46:23 -0300

    [ADDED] Store game menu options

---
+++ src/headers/game_options.h
@@ -0,0 +1,13 @@
+#ifndef _GAME_OPTIONS_H
+#define _GAME_OPTIONS_H
+
+#ifndef _STDLIB_H
+#include <stdlib.h>
+#endif
+
+static const char *MENU_OPTIONS[] = {
+  "Play", "About", "Quit", NULL
+};
+
+
+#endif
