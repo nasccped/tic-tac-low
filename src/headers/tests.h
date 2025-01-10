75f27f97fbcadd21e48b207b7916077405613153
NASCC <pedro.main.tec@gmail.com>
2025-01-10 15:55:29 -0300

    [ADDED] Tests section. Can be called by test.sh

---
+++ src/headers/tests.h
@@ -0,0 +1,29 @@
+#ifndef _TESTS_H
+#define _TESTS_H
+
+int main_test(char *);
+
+struct LinkedHashMap {
+  const char *arg;
+  void (*func)();
+  struct LinkedHashMap *next;
+};
+
+struct ArgMapping {
+  unsigned count;
+  struct LinkedHashMap *head;
+  void (*append)(struct ArgMapping *, struct LinkedHashMap *);
+};
+
+void append_lhm(struct ArgMapping *, struct LinkedHashMap *);
+
+void TEST_FUNC();
+static struct LinkedHashMap TEST = {
+  "test",
+  &TEST_FUNC,
+  &TEST
+};
+
+extern struct ArgMapping ARG_MAPPING;
+
+#endif
---
+++ src/tests.c
@@ -0,0 +1,74 @@
+#include "./headers/tests.h"
+
+#ifndef _STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef _STDIO_H
+#include <stdio.h>
+#endif
+
+#ifndef _STRING_H
+#include <string.h>
+#endif
+
+struct ArgMapping ARG_MAPPING = {
+  0, NULL, &append_lhm
+};
+
+void TEST_FUNC() {
+
+  printf("Testing...");
+}
+
+int main_test(char *arg) {
+  
+  struct ArgMapping *map = &ARG_MAPPING;
+
+  map -> append(map, &TEST);
+
+  struct LinkedHashMap *holder = map -> head;
+
+  printf("\n");
+
+  while (holder != NULL) {
+
+    if (strcmp(arg, holder -> arg) == 0) {
+
+      holder->func();
+      printf("\n");
+
+      return 0;
+    }
+
+    holder = holder -> next;
+  }
+
+  printf("The given arg (%s) could not be found!", arg);
+  printf("\n");
+
+  return 1;
+}
+
+void append_lhm(struct ArgMapping *self, struct LinkedHashMap *element) {
+
+  if (self == NULL || element == NULL)
+    return;
+
+  element -> next = NULL;
+
+  if (self -> count == 0) {
+    self -> head = element;
+    self -> count++;
+    return;
+  }
+
+  struct LinkedHashMap *hold  = self -> head;
+
+  for (int i = 0; i < self -> count; i++) {
+    hold = hold -> next;
+  }
+
+  hold -> next = element;
+  self -> count++;
+}
