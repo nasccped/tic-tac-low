443fb65ffe50afe587ed4f2181576dca0fe98e6f
NASCC <pedro.main.tec@gmail.com>
2025-01-10 10:33:31 -0300

    [UPDATED] Better readability

--- src/headers/const_vars.h
+++ src/headers/const_vars.h
@@ -1,10 +1,10 @@
 #ifndef _CONST_VARS_H
 #define _CONST_VARS_H
 
-static const char *RESET_ESCAPE = "\x1b[0m";
-static const char *BOLD_RED_NONE = "\x1b[1;31m";
-static const char *BOLD_GREEN_NONE = "\x1b[1;32m";
-static const char *BOLD_YELLOW_NONE = "\x1b[1;33m";
+static const char *RESET_ESCAPE     = "\x1b[0m"   ,
+                  *BOLD_RED_NONE    = "\x1b[1;31m",
+                  *BOLD_GREEN_NONE  = "\x1b[1;32m",
+                  *BOLD_YELLOW_NONE = "\x1b[1;33m";
 
 extern unsigned ENABLE_COLORS;
 
