5280c2385a3c026ad4cd5aea0acdc0c347ff1a4b
NASCC <pedro.main.tec@gmail.com>
2025-01-14 20:18:24 -0300

    [UPDATED] New function to P Move (update pos at table)

--- include/game/player.h
+++ include/game/player.h
@@ -2,9 +2,14 @@
 #define _PLAYER_H
 
 struct CatchPlayerMove {
+
   unsigned player_val,
            at_row    ,
            at_col    ;
+
+  struct CatchPlayerMove *(*get_move)(struct CatchPlayerMove *,
+                                     unsigned                ,
+                                     unsigned                );
 };
 
 struct CatchPlayerMove *set_player_move(struct CatchPlayerMove *,
@@ -12,7 +17,7 @@ struct CatchPlayerMove *set_player_move(struct CatchPlayerMove *,
                                         unsigned                );
 
 static struct CatchPlayerMove CATCH_PLAYER_MOVE = {
-  0, 0, 0
+  0, 0, 0, &set_player_move
 };
 
 #endif
