e6b20ba9b9bd612b65e8d53f57078fcdce6dc296
NASCC <pedro.main.tec@gmail.com>
2025-01-14 18:48:39 -0300

    [UPDATED] Adding a new test arg case

--- include/tests.h
+++ include/tests.h
@@ -18,10 +18,18 @@ struct ArgMapping {
 void append_lhm(struct ArgMapping *, struct LinkedHashMap *);
 
 void TABLE_FUNC();
+void TAKE_POS_FUNC();
+
 static struct LinkedHashMap TABLE = {
   "table",
   &TABLE_FUNC,
   &TABLE,
+};
+
+static struct LinkedHashMap TAKE_POS = {
+  "take_pos",
+  &TAKE_POS_FUNC,
+  &TAKE_POS
 };
 
 extern struct ArgMapping ARG_MAPPING;
