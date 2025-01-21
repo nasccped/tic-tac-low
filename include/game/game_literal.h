d925834a03dd748623f8e9cfe9c67f4c19c53621
NASCC <pedro.main.tec@gmail.com>
2025-01-21 19:38:54 -0300

    [UPDATED] Preparing pvp test for a single function

---
+++ include/game/game_literal.h
@@ -0,0 +1,7 @@
+#ifndef _GAME_LITERAL_H
+#define _GAME_LITERAL_H
+
+void player_vs_player_game();
+void player_vs_bot_game();
+
+#endif
---
+++ src/game_literal.c
@@ -0,0 +1,15 @@
+#include "../include/game/game_literal.h"
+
+#include <stdio.h>
+
+void player_vs_player_game() {
+
+  // TODO: implements the game here...
+  printf("  Let's play a 2P game!\n");
+}
+
+void player_vs_bot_game() {
+
+  // TODO: implements the game here...
+  printf("TODO!\n");
+}
