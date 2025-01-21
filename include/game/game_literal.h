27cc97cce6831ffcf59090ec2890659bd9857873
NASCC <pedro.main.tec@gmail.com>
2025-01-21 20:53:23 -0300

    [UPDATED] static enum for alert text control

--- include/game/game_literal.h
+++ include/game/game_literal.h
@@ -1,7 +1,34 @@
 #ifndef _GAME_LITERAL_H
 #define _GAME_LITERAL_H
 
+#define MESSAGE_MAX_LEN 150
+
+typedef enum MessageType {
+  OK_AWAITING           ,
+  OK_WINS               ,
+  OK_BREAK_EVEN         ,
+  ERR_OUT_OF_RANGE      ,
+  ERR_NON_NUMERIC       ,
+  ERR_CELL_ALREADY_TAKEN
+} MessageType;
+
+typedef struct GameMessage {
+  MessageType type;
+  char message[MESSAGE_MAX_LEN];
+  void (*updated)(struct GameMessage *,
+                  MessageType         ,
+                  char *              );
+} GameMessage;
+
 void player_vs_player_game();
 void player_vs_bot_game();
+
+void update_game_message(struct GameMessage *, MessageType, char *);
+
+static GameMessage MAIN_GAME_MESSAGE = {
+  OK_AWAITING,
+  "awaiting",
+  &update_game_message
+};
 
 #endif
