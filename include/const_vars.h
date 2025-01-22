c3df283bd9d30f8dfafc709712e1a346bb619a02
NASCC <pedro.main.tec@gmail.com>
2025-01-22 00:01:13 -0300

    [UPDATED] White color escape added

--- include/const_vars.h
+++ include/const_vars.h
@@ -7,7 +7,8 @@ static const char *RESET_ESCAPE     = "\x1b[0m"   ,
                   *BOLD_RED_NONE    = "\x1b[1;31m",
                   *BOLD_GREEN_NONE  = "\x1b[1;32m",
                   *BOLD_YELLOW_NONE = "\x1b[1;33m",
-                  *BOLD_BLUE_NONE   = "\x1b[1;34m";
+                  *BOLD_BLUE_NONE   = "\x1b[1;34m",
+                  *BOLD_WHITE_NONE  = "\x1b[1;37m";
 
 extern unsigned ENABLE_COLORS;
 
