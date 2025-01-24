1649f532b9f61ffa8fc61a82a133b9eb64be475e
NASCC <pedro.main.tec@gmail.com>
2025-01-24 12:40:21 -0300

    [UPDATED] Option display subtitles

--- src/options.c
+++ src/options.c
@@ -5,12 +5,13 @@
 
 void print_options(unsigned enable_colors, const char *options[]) {
 
+  printf("  Chose by option number:\n");
+
   // printing each option (last option as NULL is required!)
   for (int i = 0; strcmp(options[i], OPTION_END) != 0; i++) {
-    printf("  %s%d.%s %s\n", enable_colors ? BOLD_YELLOW_NONE : "",
-                             i + 1                                ,
-                             enable_colors ? RESET_ESCAPE : ""    ,
-                             options[i]
-    );
+    printf("    %s%d.%s %s\n", enable_colors ? BOLD_YELLOW_NONE : "",
+                               i + 1                                ,
+                               enable_colors ? RESET_ESCAPE : ""    ,
+                               options[i]                           );
   }
 }
