197ece3b454464b1d41d430c91220d7404f4a022
NASCC <pedro.main.tec@gmail.com>
2025-01-24 11:20:29 -0300

    [VISUAL FIX] .

--- src/options.c
+++ src/options.c
@@ -7,7 +7,7 @@ void print_options(unsigned enable_colors, const char *options[]) {
 
   // printing each option (last option as NULL is required!)
   for (int i = 0; strcmp(options[i], OPTION_END) != 0; i++) {
-    printf("  %s%d%s. %s\n", enable_colors ? BOLD_YELLOW_NONE : "",
+    printf("  %s%d.%s %s\n", enable_colors ? BOLD_YELLOW_NONE : "",
                              i + 1                                ,
                              enable_colors ? RESET_ESCAPE : ""    ,
                              options[i]
