3f0b4b633aaf0d993e707f0473bed2246d5a1c39
NASCC <pedro.main.tec@gmail.com>
2025-01-27 17:40:43 -0300

    [UPDATED] Commenting code (6/??)

--- src/player.c
+++ src/player.c
@@ -1,17 +1,37 @@
 #include <stdlib.h>
 #include "../include/game/player.h"
 
-struct CatchPlayerMove *set_player_move(CatchPlayerMove *dest, unsigned int player, unsigned pos) {
+/*
+ * A function to get given values and update struct variables based
+ * on these values.
+ *
+ * Input:
+ *    - CatchPlayerMove struct pointer (dest) to insert values
+ *    - unsigned value (player) to hold player symbol
+ *    - unsigned value (pos) to hold the move position
+ *
+ * Output:
+ *    Will return the dest pointer if it's success. Don't worry! The
+ *    pointer won't be lost. The pointer return is used just to check
+ *    success.
+ * */
+struct CatchPlayerMove *set_player_move(CatchPlayerMove *dest,
+                                        unsigned player,
+                                        unsigned pos) {
 
+  // if invalid pos
   if (pos < 1 || pos > 9)
     return NULL;
 
+  // if invalid player
   if (player < 1 || player > 2)
     return NULL;
 
+  // default values
   unsigned row = 2,
            col = 0;
 
+  // loop to catch 'row' and 'col' based on 'pos'
   for (int i = 0; i < (pos - 1); i++) {
 
     if (col == 2) {
@@ -24,7 +44,8 @@ struct CatchPlayerMove *set_player_move(CatchPlayerMove *dest, unsigned int play
 
     col++;
   }
-  
+
+  // update pointer values + return it
   dest -> at_col     = col   ;
   dest -> at_row     = row   ;
   dest -> player_val = player;
