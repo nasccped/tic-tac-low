adb4a369a22033814dec9d38a5949ec8f43d8994
NASCC <pedro.main.tec@gmail.com>
2025-01-27 17:34:30 -0300

    [UPDATED] Resuming commenting (5/??)

--- src/options.c
+++ src/options.c
@@ -3,11 +3,22 @@
 #include "../include/const_vars.h"
 #include "../include/options.h"
 
+/*
+ * Self explanatory.
+ *
+ * Input:
+ *    - unsigned value (enable_colors) for escapes trigger
+ *    - string array <char pointers array> (options)
+ *
+ * Output:
+ *    Nothing. Just prints
+ * */
 void print_options(unsigned enable_colors, const char *options[]) {
 
+  // print tip
   printf("  Chose by option number:\n");
 
-  // printing each option (last option as NULL is required!)
+  // printing each option
   for (int i = 0; strcmp(options[i], OPTION_END) != 0; i++) {
 
     printf("    %s%d.%s %s\n",
@@ -15,6 +26,5 @@ void print_options(unsigned enable_colors, const char *options[]) {
            i + 1                                ,
            enable_colors ? RESET_ESCAPE : ""    ,
            options[i]                           );
-
   }
 }
